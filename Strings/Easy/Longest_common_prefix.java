cc25c3c25768b5d343254cbcb7e8669fdbccf964
ms-maheswari <mahes7439@gmail.com>
2024-09-20 14:12:57 +0530

    first commit

--- Strings/Easy/Longest_common_prefix.java
+++ Strings/Easy/Longest_common_prefix.java
@@ -5,19 +5,26 @@ import java.util.Arrays;
 public class Longest_common_prefix {
 
     public static String longest_prefix(String str[]){
+
+        // Sorting the array str[]
         Arrays.sort(str);
         int n = str.length;
-        String s1 = str[0];
-        String s2 = str[n-1];
 
-        String op = "";
-        for(int i=0; i<s1.length(); i++){
-            if(s1.charAt(i) == s2.charAt(i))
-               op = op+s1.charAt(i);
-            else 
-               break;
+        String s1 = str[0];  // First string in the sorted array
+        String s2 = str[n - 1];  // Last string in the sorted array
+
+        String op = "";  // Variable to store the common prefix
+
+        // Loop through the characters of the first string
+        for (int i = 0; i < s1.length(); i++) {
+            // If the characters of both strings are the same, add to the prefix
+            if (s1.charAt(i) == s2.charAt(i))
+                op = op + s1.charAt(i);
+            else
+                break;  // Stop when characters no longer match
         }
-        return op;
+
+        return op;  // Return the longest common prefix
     }
     public static void main(String[] args) {
         String str[] = {"flower","flow","flight"};
---
+++ Strings/Easy/Valid_Palindrome.java
@@ -0,0 +1,30 @@
+package Easy;
+
+public class Valid_Palindrome {
+    public static boolean isPalindrome(String s) {
+        // Convert the string to lowercase and remove all non-alphanumeric characters
+        String str = s.toLowerCase().replaceAll("[^a-zA-Z0-9]", "");
+        
+        // Initialize two pointers
+        int left = 0;
+        int right = str.length() - 1;
+        
+        
+        while (left < right) {
+            // If characters don't match, it's not a palindrome
+            if (str.charAt(left) != str.charAt(right)) {
+                return false;  
+            }
+            left++;  // Move left pointer forward
+            right--;  // Move right pointer backward
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String s = "A man, a plan, a canal: Panama";
+        System.out.print(isPalindrome(s) ? "Valid Palindrome": "Not valid Palindrome");
+    }
+}
+
+// Input: s = "A man, a plan, a canal: Panama"
+// Output: Valid Palindrome
