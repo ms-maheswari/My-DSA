3b8351e0ebe5a8e97e0de3d55b1d4bc9602a30a9
ms-maheswari <mahes7439@gmail.com>
2025-04-30 11:55:38 +0530

    updated code

--- Array/Medium Array Programs/Set_Matrix_Zeros.java
+++ Array/Medium Array Programs/Set_Matrix_Zeros.java
@@ -1,26 +1,6 @@
 //  If an element is 0, set its entire row and column to 0's
 
 public class Set_Matrix_Zeros {
-    public static void main(String[] args) {
-        int[][] matrix = {
-            {1, 1, 1},
-            {1, 0, 1},
-            {1, 1, 1}
-        };
-
-        setZeroes(matrix);
-
-        // Print the modified matrix
-        for (int i = 0; i < matrix.length; i++) // "matrix.length" gives the number of rows
-        {
-            for (int j = 0; j < matrix[0].length; j++) //"matrix[0].length" gives the number of columns 
-            {
-                System.out.print(matrix[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
-
     public static void setZeroes(int[][] matrix) {
         int rowLength = matrix.length;
         int colLength = matrix[0].length;
@@ -47,4 +27,26 @@ public class Set_Matrix_Zeros {
             }
         }
     }
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 1, 1},
+            {1, 0, 1},
+            {1, 1, 1}
+        };
+
+        setZeroes(matrix);
+
+        // Print the modified matrix
+        for (int i = 0; i < matrix.length; i++) // "matrix.length" gives the number of rows
+        {
+            for (int j = 0; j < matrix[0].length; j++) //"matrix[0].length" gives the number of columns 
+            {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }   
 }
+
+// Input : [[1,1,1],[1,0,1],[1,1,1]]
+// Output : [[1,0,1],[0,0,0],[1,0,1]]
